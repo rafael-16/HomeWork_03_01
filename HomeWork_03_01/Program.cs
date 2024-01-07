2039d7525de1b120d1b6e454d7b5bf7c58c23b5b
Рафаэль Мухамедьяров <Rafael17IS16@gmail.com>
2024-01-07 14:44:46 +0300

    Add project files.

---
+++ HomeWork_03_01/Program.cs
@@ -0,0 +1,8 @@
+﻿using HomeWork_03_01;
+
+Web_Site web_site = new Web_Site();
+web_site.Data_Entry();
+
+Console.WriteLine("\n\tИнформация о веб-сайте");
+web_site.Data_Output();
+
---
+++ HomeWork_03_01/Web_Site.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_03_01
+{
+    class Web_Site
+    {
+        private string name = string.Empty;
+        private string url = string.Empty;
+        private string description = string.Empty;
+        private string ipAdress = string.Empty;
+
+        public void SetName(string name) { this.name = name; }
+        public string GetName() { return this.name; }
+        public void SetUrl(string url) { this.url = url; }
+        public string GetUrl() { return this.url; }
+        public void SetDescription(string description) { this.description = description;}
+        public string GetDescription() { return this.description;}
+        public void SetIpAdress(string ipAdress) {  this.ipAdress = ipAdress;}
+        public string GetIpAdress() {  return this.ipAdress; }
+
+        public void Data_Entry()
+        {
+            Console.Write("Введите название сайта: ");
+            name = Console.ReadLine()!;
+
+            Console.Write("Введите путь к сайту: ");
+            url = Console.ReadLine()!;
+
+            Console.Write("Опишите о чем сайт: ");
+            description = Console.ReadLine()!;
+
+            Console.Write("Введите ip-adress: ");
+            ipAdress = Console.ReadLine()!;
+        }
+
+        public void Data_Output()
+        {
+            Console.WriteLine("Имя сайта: " +  name);
+            Console.WriteLine("Путь к сайту: " +  url);
+            Console.WriteLine("Описание сайта: " + description);
+            Console.WriteLine("IP - adress: " +  ipAdress);
+        }
+
+    }
+
+}
